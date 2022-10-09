4817fc947d2b05a03b788ba76af2a1d4201a7d56
MaksimKudryavtsev <maxd4567@gmail.com>
2022-10-10 02:43:15 +0300

    implement Basket.API microservice

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using Basket.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 namespace Basket.API
 {
     public class Program
@@ -7,11 +10,21 @@ namespace Basket.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration
+                    .GetValue<string>("CacheSettings:ConnectionString");
+            });
+
+            builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+            });
 
             var app = builder.Build();
 
@@ -19,7 +32,7 @@ namespace Basket.API
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
             }
 
             app.UseAuthorization();
