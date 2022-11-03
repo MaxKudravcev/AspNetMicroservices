d06d0ead130bc55219c74694060ff8ad3b6e59e6
MaksimKudryavtsev <maxd4567@gmail.com>
2022-11-03 22:39:18 +0300

    implement otdering.api and ordering.infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistance/OrderContextSeed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seeded the database associated with {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order()
+                {
+                    UserName = "swn",
+                    FirstName = "Max",
+                    LastName = "Kudryavtsev",
+                    EmailAddress = "maxd4567@gmail.com",
+                    AddressLine = "Minsk",
+                    Country = "Belarus",
+                    TotalPrice = 350
+                }
+            };
+        }
+    }
+}
