ba2d10f7a4f6962015b05faf8f09beaab3106df9
MaksimKudryavtsev <maxd4567@gmail.com>
2022-11-21 23:41:11 +0300

    implement shopping.aggregator and add docker support

--- src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -4,7 +4,7 @@ namespace Shopping.Aggregator.Extensions
 {
     public static class HttpClientExtensions
     {
-        public static async Task<T> ReadContextAs<T>(this HttpResponseMessage response)
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
         {
             if (!response.IsSuccessStatusCode)
                 throw new ApplicationException($"Something went wrong when calling the API: {response.ReasonPhrase}");
